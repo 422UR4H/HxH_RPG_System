6620061c3537e26194095c55355468e16594ee41
422UR4H <tiagosindra@id.uff.br>
2025-01-20 18:46:13 -0300

    fix: add alignment in profile

--- internal/domain/entity/sheet/character_profile.go
+++ internal/domain/entity/sheet/character_profile.go
@@ -5,6 +5,7 @@ import "time"
 type CharacterProfile struct {
 	NickName         string
 	FullName         string
+	Alignment        string
 	Description      string
 	BriefDescription string
 	Birthday         time.Time
