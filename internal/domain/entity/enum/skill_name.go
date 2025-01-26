c0a829d55fe6b60d27e44475bf8afe2dcd235ef8
422UR4H <tiagosindra@id.uff.br>
2025-01-26 18:58:36 -0300

    feat: add parse string of skillName enum

--- internal/domain/entity/enum/skill_name.go
+++ internal/domain/entity/enum/skill_name.go
@@ -64,3 +64,75 @@ const (
 	Focus
 	WillPower
 )
+
+func (sn SkillName) String() string {
+	switch sn {
+	case Vitality:
+		return "Vitality"
+	case Resistance:
+		return "Resistance"
+	case Breath:
+		return "Breath"
+	case Heal:
+		return "Heal"
+	case DefenseSkill:
+		return "DefenseSkill"
+	case Climb:
+		return "Climb"
+	case Push:
+		return "Push"
+	case Grab:
+		return "Grab"
+	case CarryCapacity:
+		return "CarryCapacity"
+	case Run:
+		return "Run"
+	case Swim:
+		return "Swim"
+	case Jump:
+		return "Jump"
+	case Dodge:
+		return "Dodge"
+	case Accelerate:
+		return "Accelerate"
+	case Brake:
+		return "Brake"
+	case AttackSpeed:
+		return "AttackSpeed"
+	case Feint:
+		return "Feint"
+	case Acrobatics:
+		return "Acrobatics"
+	case Sneak:
+		return "Sneak"
+	case Reflex:
+		return "Reflex"
+	case Accuracy:
+		return "Accuracy"
+	case Stealth:
+		return "Stealth"
+	case SleightOfHand:
+		return "SleightOfHand"
+	case Vision:
+		return "Vision"
+	case Hearing:
+		return "Hearing"
+	case Smell:
+		return "Smell"
+	case Tact:
+		return "Tact"
+	case Taste:
+		return "Taste"
+	case Balance:
+		return "Balance"
+	case Intuition:
+		return "Intuition"
+	case Nen:
+		return "Nen"
+	case Focus:
+		return "Focus"
+	case WillPower:
+		return "WillPower"
+	}
+	return "Unknown"
+}
