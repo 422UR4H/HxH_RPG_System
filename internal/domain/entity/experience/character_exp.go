2466ae67a7e356fcd23ac2b8e62561e5b0354030
422UR4H <tiagosindra@id.uff.br>
2025-01-26 18:58:15 -0300

    feat: add GetExpPoints and lvl of characterExp

--- internal/domain/entity/experience/character_exp.go
+++ internal/domain/entity/experience/character_exp.go
@@ -12,3 +12,11 @@ func NewCharacterExp(exp Exp) *CharacterExp {
 func (ce *CharacterExp) EndCascadeUpgrade(exp int) {
 	ce.exp.IncreasePoints(exp)
 }
+
+func (ce *CharacterExp) GetExpPoints() int {
+	return ce.exp.GetPoints()
+}
+
+func (ce *CharacterExp) GetLevel() int {
+	return ce.exp.GetLevel()
+}
