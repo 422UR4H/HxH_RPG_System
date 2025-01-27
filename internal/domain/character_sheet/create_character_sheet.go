b9ae045160c1ac38b93895bbd1c46dc2835e0210
422UR4H <tiagosindra@id.uff.br>
2025-01-26 21:32:24 -0300

    feat: init create sheet uc

--- internal/domain/character_sheet/create_character_sheet.go
+++ internal/domain/character_sheet/create_character_sheet.go
@@ -1,1 +1,30 @@
 package charactersheet
+
+import "github.com/422UR4H/HxH_RPG_System/internal/domain/entity/sheet"
+
+type ICreateCharacterSheet interface {
+	CreateCharacterSheet() *sheet.CharacterSheet
+}
+
+type CreateCharacterSheetUC struct {
+	// repo
+}
+
+func NewCreateCharacterSheetUC() *CreateCharacterSheetUC {
+	return &CreateCharacterSheetUC{
+		// repo
+	}
+}
+
+type CreateCharacterSheetInput struct {
+	profile sheet.CharacterProfile
+	set     sheet.TalentByCategorySet
+}
+
+func (uc *CreateCharacterSheetUC) CreateCharacterSheet(
+	input CreateCharacterSheetInput,
+) *sheet.CharacterSheet {
+	factory := sheet.NewCharacterSheetFactory()
+	characterSheet := factory.Build(input.profile, input.set)
+	return characterSheet
+}
