666303c484b75950dd6eddf2dc1e3ae0a72628f2
422UR4H <tiagosindra@id.uff.br>
2025-01-27 20:16:51 -0300

    refactor: order category names

--- internal/domain/entity/enum/category_name.go
+++ internal/domain/entity/enum/category_name.go
@@ -4,27 +4,27 @@ type CategoryName int
 
 const (
 	Reinforcement = iota
-	Emission
 	Transmutation
-	Manipulation
 	Materialization
 	Specialization
+	Manipulation
+	Emission
 )
 
 func (cn CategoryName) String() string {
 	switch cn {
 	case Reinforcement:
 		return "Reinforcement"
-	case Emission:
-		return "Emission"
 	case Transmutation:
 		return "Transmutation"
-	case Manipulation:
-		return "Manipulation"
 	case Materialization:
 		return "Materialization"
 	case Specialization:
 		return "Specialization"
+	case Manipulation:
+		return "Manipulation"
+	case Emission:
+		return "Emission"
 	}
 	return "Unknown"
 }
