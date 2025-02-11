8cf16402b0c2207511be9411e6dfc63b56dd7695
422UR4H <tiagosindra@id.uff.br>
2025-02-10 22:28:42 -0300

    fix: error handling, levelOf and improve code

--- internal/domain/entity/proficiency/proficiency_manager.go
+++ internal/domain/entity/proficiency/proficiency_manager.go
@@ -7,73 +7,68 @@ import (
 )
 
 type Manager struct {
-	commonProficiencies map[enum.WeaponName]*Proficiency
 	personProficiencies map[string]*PersonProficiency
-	// TODO: refactor buffs to accept personProficiencies
-	buffs map[enum.WeaponName]int
+	commonProficiencies map[enum.WeaponName]*Proficiency
+	buffs               map[enum.WeaponName]int
 }
 
 func NewManager() *Manager {
 	return &Manager{
-		commonProficiencies: make(map[enum.WeaponName]*Proficiency),
 		personProficiencies: make(map[string]*PersonProficiency),
+		commonProficiencies: make(map[enum.WeaponName]*Proficiency),
 		buffs:               make(map[enum.WeaponName]int),
 	}
 }
 
 func (m *Manager) Get(name enum.WeaponName) (IProficiency, error) {
-	for _, proficiency := range m.personProficiencies {
-		if proficiency.ContainsWeapon(name) {
-			return proficiency, nil
+	for _, prof := range m.personProficiencies {
+		if prof.ContainsWeapon(name) {
+			return prof, nil
 		}
 	}
-	if proficiency, ok := m.commonProficiencies[name]; ok {
-		return proficiency, nil
+	if prof, ok := m.commonProficiencies[name]; ok {
+		return prof, nil
 	}
 	return nil, errors.New("proficiency not found")
 }
 
 func (m *Manager) GetExpPointsOf(name enum.WeaponName) (int, error) {
-	proficiency, err := m.Get(name)
-	if err == nil {
+	prof, err := m.Get(name)
+	if err != nil {
 		return 0, err
 	}
-	return proficiency.GetExpPoints(), nil
+	return prof.GetExpPoints(), nil
 }
 
 func (m *Manager) GetLevelOf(name enum.WeaponName) (int, error) {
-	proficiency, err := m.Get(name)
-	if err == nil {
+	prof, err := m.Get(name)
+	if err != nil {
 		return 0, err
 	}
-	lvl := proficiency.GetLevel()
-
-	if buff, ok := m.buffs[name]; ok {
-		lvl += buff
-	}
-	return lvl, nil
+	return prof.GetLevel(), nil
 }
 
-// TODO: validate if is necessary to sum the buffs here
-// func (m *Manager) GetValueForTestOf(name enum.WeaponName) int {
-// 	proficiency := m.Get(name)
-// 	if proficiency == nil {
-// 		return 0
-// 	}
-// 	testVal := proficiency.GetValueForTest()
-
-// 	if buff, ok := m.buffs[name]; ok {
-// 		testVal += buff
-// 	}
-// 	return testVal
-// }
-
-func (m *Manager) IncreaseExp(exp int, name enum.WeaponName) (int, error) {
-	proficiency, err := m.Get(name)
-	if err == nil {
+func (m *Manager) GetValueForTestOf(name enum.WeaponName) (int, error) {
+	prof, err := m.Get(name)
+	if err != nil {
 		return 0, err
 	}
-	return proficiency.CascadeUpgradeTrigger(exp), nil
+	// TODO: validate this
+	// testVal := prof.GetValueForTest()
+	testVal := prof.GetLevel()
+
+	if buff, ok := m.buffs[name]; ok {
+		testVal += buff
+	}
+	return testVal, nil
+}
+
+func (m *Manager) IncreaseExp(exp int, name enum.WeaponName) (int, error) {
+	prof, err := m.Get(name)
+	if err != nil {
+		return 0, err
+	}
+	return prof.CascadeUpgradeTrigger(exp), nil
 }
 
 func (m *Manager) SetBuff(name enum.WeaponName, value int) int { //, int) {
