36f5b56a822f4b646ae49d79b53fe2c504c54787
422UR4H <tiagosindra@id.uff.br>
2025-02-12 21:22:23 -0300

    feat: add ToString func n character profile

--- internal/domain/entity/sheet/character_profile.go
+++ internal/domain/entity/sheet/character_profile.go
@@ -10,3 +10,16 @@ type CharacterProfile struct {
 	BriefDescription string
 	Birthday         time.Time
 }
+
+func (cp *CharacterProfile) ToString() string {
+	profile := "Nick: " + cp.NickName + "\n"
+	profile += "Name: " + cp.FullName + "\n"
+	profile += "Alignment: " + cp.Alignment + " | "
+	profile += "Birthday: " + cp.Birthday.String() + "\n"
+
+	briefDesc := cp.BriefDescription
+	if briefDesc != "" {
+		profile += "Brief Description: " + briefDesc + "\n"
+	}
+	return profile
+}
