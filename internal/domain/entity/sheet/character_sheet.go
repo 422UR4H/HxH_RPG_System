cf0ab576d78c14025299840c9352fb52cde0797d
422UR4H <tiagosindra@id.uff.br>
2025-02-13 21:55:34 -0300

    feat: add ToString func in sheets

--- internal/domain/entity/sheet/character_sheet.go
+++ internal/domain/entity/sheet/character_sheet.go
@@ -1,6 +1,8 @@
 package sheet
 
 import (
+	"fmt"
+
 	"github.com/422UR4H/HxH_RPG_System/internal/domain/entity/ability"
 	"github.com/422UR4H/HxH_RPG_System/internal/domain/entity/attribute"
 	"github.com/422UR4H/HxH_RPG_System/internal/domain/entity/enum"
@@ -113,4 +115,95 @@ func (cs *CharacterSheet) GetExpPointsOfCategory(name enum.CategoryName) (int, e
 
 func (cs *CharacterSheet) GetExpPoints() int {
 	return cs.ability.GetCharacterExpPoints()
+}
+
+func (cs *CharacterSheet) ToString() string {
+	sheet := "=============================\n"
+	sheet += cs.profile.ToString()
+
+	sheet += "CHARACTER LVL: " + fmt.Sprint(cs.ability.GetCharacterLevel()) +
+		"\t| Points: " + fmt.Sprint(cs.ability.GetCharacterPoints()) +
+		"\t| Talent: " + fmt.Sprint(cs.ability.GetTalentLevel()) + "\n"
+
+	sheet += "Exp Total: " + fmt.Sprint(cs.ability.GetCharacterExpPoints()) +
+		"\t| Exp: " + fmt.Sprint(cs.ability.GetCharacterCurrentExp()) +
+		" / " + fmt.Sprint(cs.ability.GetCharacterNextLvlBaseExp()) + "\n"
+	sheet += "-----------------------------\n"
+
+	physicals, _ := cs.ability.Get(enum.Physicals)
+	sheet += "PHYSICALS LVL: " + fmt.Sprint(physicals.GetLevel()) +
+		"\t| Bonus: " + fmt.Sprintf("%.1f\n", physicals.GetBonus())
+
+	sheet += "Exp Total: " + fmt.Sprint(physicals.GetExpPoints()) +
+		"\t| Exp: " + fmt.Sprint(physicals.GetCurrentExp()) +
+		" / " + fmt.Sprint(physicals.GetNextLvlBaseExp()) + "\n"
+
+	physicalsLvl := cs.attribute.GetPhysicalsLevel()
+	physicalsExp := cs.attribute.GetPhysicalsExpPoints()
+	physicalsCurrExp := cs.attribute.GetPhysicalsCurrentExp()
+	physicalsNextLvlExp := cs.attribute.GetPhysicalsNextLvlBaseExp()
+	sortedAttrNames := []enum.AttributeName{
+		enum.Resistance, enum.Strength, enum.Agility, enum.ActionSpeed,
+		enum.Flexibility, enum.Dexterity, enum.Sense, enum.Constitution,
+	}
+	for _, name := range sortedAttrNames {
+		lvl := physicalsLvl[name]
+		exp := physicalsExp[name]
+		currExp := physicalsCurrExp[name]
+		nextLvlExp := physicalsNextLvlExp[name]
+		sheet += name.String() + " Lvl: " + fmt.Sprint(lvl) +
+			"\t| Exp Total: " + fmt.Sprint(exp) +
+			"\t| Exp: " + fmt.Sprint(currExp) +
+			" / " + fmt.Sprint(nextLvlExp) + "\n"
+	}
+	sheet += "-----------------------------\n"
+
+	skillsLvl := cs.skill.GetPhysicalsLevel()
+	skillsExp := cs.skill.GetPhysicalsExpPoints()
+	skillsCurrExp := cs.skill.GetPhysicalsCurrentExp()
+	skillsNextLvlExp := cs.skill.GetPhysicalsNextLvlBaseExp()
+	sortSkillNames := []enum.SkillName{
+		enum.Vitality, enum.Energy, enum.Defense,
+		enum.Push, enum.Grab, enum.CarryCapacity,
+		enum.Velocity, enum.Accelerate, enum.Brake,
+		enum.AttackSpeed, enum.Repel, enum.Feint,
+		enum.Acrobatics, enum.Evasion, enum.Sneak,
+		enum.Reflex, enum.Accuracy, enum.Stealth,
+		enum.Vision, enum.Hearing, enum.Smell,
+		enum.Tact, enum.Taste, enum.Balance,
+		enum.Heal, enum.Breath, enum.Tenacity,
+	}
+	for _, name := range sortSkillNames {
+		lvl := skillsLvl[name]
+		exp := skillsExp[name]
+		if lvl == 0 && lvl == exp {
+			continue
+		}
+		currExp := skillsCurrExp[name]
+		nextLvlExp := skillsNextLvlExp[name]
+
+		sheet += name.String() + " Lvl: " + fmt.Sprint(lvl) +
+			"\t| Exp Total: " + fmt.Sprint(exp) +
+			"\t| Exp: " + fmt.Sprint(currExp) +
+			" / " + fmt.Sprint(nextLvlExp) + "\n"
+	}
+	sheet += "-----------------------------\n"
+
+	mentals, _ := cs.ability.Get(enum.Mentals)
+	sheet += "MENTALS LVL: " + fmt.Sprint(mentals.GetLevel()) +
+		"\t| Bonus: " + fmt.Sprintf("%.1f\n", mentals.GetBonus())
+
+	sheet += "Exp Total: " + fmt.Sprint(mentals.GetExpPoints()) +
+		"\t| Exp: " + fmt.Sprint(mentals.GetCurrentExp()) +
+		" / " + fmt.Sprint(mentals.GetNextLvlAggregateExp()) + "\n"
+	sheet += "-----------------------------\n"
+
+	statusList := cs.status.GetAllStatus()
+	for name, status := range statusList {
+		sheet += name.String() + ": Min " + fmt.Sprint(status.GetMin()) +
+			"\t| : " + fmt.Sprint(status.GetCurrent()) +
+			" / " + fmt.Sprint(status.GetMax()) + "\n"
+	}
+	sheet += "=============================\n"
+	return sheet
 }
--- internal/domain/entity/sheet/half_sheet.go
+++ internal/domain/entity/sheet/half_sheet.go
@@ -1,6 +1,8 @@
 package sheet
 
 import (
+	"fmt"
+
 	"github.com/422UR4H/HxH_RPG_System/internal/domain/entity/ability"
 	"github.com/422UR4H/HxH_RPG_System/internal/domain/entity/attribute"
 	"github.com/422UR4H/HxH_RPG_System/internal/domain/entity/enum"
@@ -33,48 +35,139 @@ func NewHalfSheet(
 	}
 }
 
-func (cs *HalfSheet) GetValueForTestOfSkill(name enum.SkillName) (int, error) {
-	return cs.skill.GetValueForTestOf(name)
+func (hs *HalfSheet) GetValueForTestOfSkill(name enum.SkillName) (int, error) {
+	return hs.skill.GetValueForTestOf(name)
 }
 
-func (cs *HalfSheet) IncreaseExpForSkill(
+func (hs *HalfSheet) IncreaseExpForSkill(
 	points int, name enum.SkillName,
 ) (int, error) {
-	return cs.skill.IncreaseExp(points, name)
+	return hs.skill.IncreaseExp(points, name)
 }
 
-func (cs *HalfSheet) GetMaxOfStatus(name enum.StatusName) (int, error) {
-	return cs.status.GetMaxOf(name)
+func (hs *HalfSheet) GetMaxOfStatus(name enum.StatusName) (int, error) {
+	return hs.status.GetMaxOf(name)
 }
 
-func (cs *HalfSheet) GetMinOfStatus(name enum.StatusName) (int, error) {
-	return cs.status.GetMinOf(name)
+func (hs *HalfSheet) GetMinOfStatus(name enum.StatusName) (int, error) {
+	return hs.status.GetMinOf(name)
 }
 
-func (cs *HalfSheet) GetLevelOfAbility(name enum.AbilityName) (int, error) {
-	return cs.ability.GetLevelOf(name)
+func (hs *HalfSheet) GetLevelOfAbility(name enum.AbilityName) (int, error) {
+	return hs.ability.GetLevelOf(name)
 }
 
-func (cs *HalfSheet) GetLevelOfAttribute(name enum.AttributeName) (int, error) {
-	return cs.attribute.GetLevelOf(name)
+func (hs *HalfSheet) GetLevelOfAttribute(name enum.AttributeName) (int, error) {
+	return hs.attribute.GetLevelOf(name)
 }
 
-func (cs *HalfSheet) GetLevelOfSkill(name enum.SkillName) (int, error) {
-	return cs.skill.GetLevelOf(name)
+func (hs *HalfSheet) GetLevelOfSkill(name enum.SkillName) (int, error) {
+	return hs.skill.GetLevelOf(name)
 }
 
-func (cs *HalfSheet) GetExpPointsOfAbility(name enum.AbilityName) (int, error) {
-	return cs.ability.GetExpPointsOf(name)
+func (hs *HalfSheet) GetExpPointsOfAbility(name enum.AbilityName) (int, error) {
+	return hs.ability.GetExpPointsOf(name)
 }
 
-func (cs *HalfSheet) GetExpPointsOfAttribute(name enum.AttributeName) (int, error) {
-	return cs.attribute.GetExpPointsOf(name)
+func (hs *HalfSheet) GetExpPointsOfAttribute(name enum.AttributeName) (int, error) {
+	return hs.attribute.GetExpPointsOf(name)
 }
 
-func (cs *HalfSheet) GetExpPointsOfSkill(name enum.SkillName) (int, error) {
-	return cs.skill.GetExpPointsOf(name)
+func (hs *HalfSheet) GetExpPointsOfSkill(name enum.SkillName) (int, error) {
+	return hs.skill.GetExpPointsOf(name)
 }
 
-func (cs *HalfSheet) GetExpPoints() int {
-	return cs.ability.GetCharacterExpPoints()
+func (hs *HalfSheet) GetExpPoints() int {
+	return hs.ability.GetCharacterExpPoints()
+}
+
+func (hs *HalfSheet) ToString() string {
+	sheet := "=============================\n"
+	sheet += hs.profile.ToString()
+
+	sheet += "CHARACTER LVL: " + fmt.Sprint(hs.ability.GetCharacterLevel()) +
+		"\t| Points: " + fmt.Sprint(hs.ability.GetCharacterPoints()) +
+		"\t| Talent: " + fmt.Sprint(hs.ability.GetTalentLevel()) + "\n"
+
+	sheet += "Exp Total: " + fmt.Sprint(hs.ability.GetCharacterExpPoints()) +
+		"\t| Exp: " + fmt.Sprint(hs.ability.GetCharacterCurrentExp()) +
+		" / " + fmt.Sprint(hs.ability.GetCharacterNextLvlBaseExp()) + "\n"
+	sheet += "-----------------------------\n"
+
+	physicals, _ := hs.ability.Get(enum.Physicals)
+	sheet += "PHYSICALS LVL: " + fmt.Sprint(physicals.GetLevel()) +
+		"\t| Bonus: " + fmt.Sprintf("%.1f\n", physicals.GetBonus())
+
+	sheet += "Exp Total: " + fmt.Sprint(physicals.GetExpPoints()) +
+		"\t| Exp: " + fmt.Sprint(physicals.GetCurrentExp()) +
+		" / " + fmt.Sprint(physicals.GetNextLvlBaseExp()) + "\n"
+
+	physicalsLvl := hs.attribute.GetPhysicalsLevel()
+	physicalsExp := hs.attribute.GetPhysicalsExpPoints()
+	physicalsCurrExp := hs.attribute.GetPhysicalsCurrentExp()
+	physicalsNextLvlExp := hs.attribute.GetPhysicalsNextLvlBaseExp()
+	sortAttrNames := []enum.AttributeName{
+		enum.Resistance, enum.Strength, enum.Agility, enum.ActionSpeed,
+		enum.Flexibility, enum.Dexterity, enum.Sense, enum.Constitution,
+	}
+	for _, name := range sortAttrNames {
+		lvl := physicalsLvl[name]
+		exp := physicalsExp[name]
+		currExp := physicalsCurrExp[name]
+		nextLvlExp := physicalsNextLvlExp[name]
+		sheet += name.String() + "\tLvl: " + fmt.Sprint(lvl) +
+			"\t| Exp Total: " + fmt.Sprint(exp) +
+			"\t| Exp: " + fmt.Sprint(currExp) +
+			" / " + fmt.Sprint(nextLvlExp) + "\n"
+	}
+	sheet += "-----------------------------\n"
+
+	skillsLvl := hs.skill.GetPhysicalsLevel()
+	skillsExp := hs.skill.GetPhysicalsExpPoints()
+	skillsCurrExp := hs.skill.GetPhysicalsCurrentExp()
+	skillsNextLvlExp := hs.skill.GetPhysicalsNextLvlBaseExp()
+	sortSkillNames := []enum.SkillName{
+		enum.Vitality, enum.Energy, enum.Defense,
+		enum.Push, enum.Grab, enum.CarryCapacity,
+		enum.Velocity, enum.Accelerate, enum.Brake,
+		enum.AttackSpeed, enum.Repel, enum.Feint,
+		enum.Acrobatics, enum.Evasion, enum.Sneak,
+		enum.Reflex, enum.Accuracy, enum.Stealth,
+		enum.Vision, enum.Hearing, enum.Smell,
+		enum.Tact, enum.Taste, enum.Balance,
+		enum.Heal, enum.Breath, enum.Tenacity,
+	}
+	for _, name := range sortSkillNames {
+		lvl := skillsLvl[name]
+		exp := skillsExp[name]
+		if lvl == 0 && lvl == exp {
+			continue
+		}
+		currExp := skillsCurrExp[name]
+		nextLvlExp := skillsNextLvlExp[name]
+
+		sheet += name.String() + "\tLvl: " + fmt.Sprint(lvl) +
+			"\t| Exp Total: " + fmt.Sprint(exp) +
+			"\t| Exp: " + fmt.Sprint(currExp) +
+			" / " + fmt.Sprint(nextLvlExp) + "\n"
+	}
+	sheet += "-----------------------------\n"
+
+	mentals, _ := hs.ability.Get(enum.Mentals)
+	sheet += "MENTALS LVL: " + fmt.Sprint(mentals.GetLevel()) +
+		"\t| Bonus: " + fmt.Sprintf("%.1f\n", mentals.GetBonus())
+
+	sheet += "Exp Total: " + fmt.Sprint(mentals.GetExpPoints()) +
+		"\t| Exp: " + fmt.Sprint(mentals.GetCurrentExp()) +
+		" / " + fmt.Sprint(mentals.GetNextLvlAggregateExp()) + "\n"
+	sheet += "-----------------------------\n"
+
+	statusList := hs.status.GetAllStatus()
+	for name, status := range statusList {
+		sheet += name.String() + ": Min " + fmt.Sprint(status.GetMin()) +
+			"\t| : " + fmt.Sprint(status.GetCurrent()) +
+			" / " + fmt.Sprint(status.GetMax()) + "\n"
+	}
+	sheet += "=============================\n"
+	return sheet
 }
