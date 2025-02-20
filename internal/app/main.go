0f95f2acefba7ab7eba5b4c9993b652d52a2da39
422UR4H <tiagosindra@id.uff.br>
2025-02-19 23:05:40 -0300

    feat: add sync map to character classes

--- internal/app/main.go
+++ internal/app/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	cc "github.com/422UR4H/HxH_RPG_System/internal/domain/entity/character_class"
 	"github.com/422UR4H/HxH_RPG_System/internal/domain/entity/enum"
 	"github.com/422UR4H/HxH_RPG_System/internal/domain/entity/sheet"
 )
 
-var characterClasses map[enum.CharacterClassName]cc.CharacterClass
+var characterClasses sync.Map
+
+// TODO: remove or handle after balancing
 var charClassSheets map[enum.CharacterClassName]*sheet.CharacterSheet
 
 func main() {
-	characterClasses = make(map[enum.CharacterClassName]cc.CharacterClass)
 	charClassSheets = make(map[enum.CharacterClassName]*sheet.CharacterSheet)
 	InitCharacterClasses()
 }
@@ -20,9 +22,13 @@ func main() {
 func InitCharacterClasses() {
 	factory := sheet.NewCharacterSheetFactory()
 	ccFactory := cc.NewCharacterClassFactory()
-	characterClasses = ccFactory.Build()
+	for name, class := range ccFactory.Build() {
+		characterClasses.Store(name, class)
+	}
 
-	for name, class := range characterClasses {
+	characterClasses.Range(func(key, value interface{}) bool {
+		name := key.(enum.CharacterClassName)
+		class := value.(cc.CharacterClass)
 		profile := sheet.CharacterProfile{
 			NickName:         name.String(),
 			Alignment:        class.Profile.Alignment,
@@ -42,23 +48,32 @@ func InitCharacterClasses() {
 		)
 		if err != nil {
 			fmt.Println(err)
-			continue
 		}
 		newClass := factory.Build(profile, set, &class)
 		charClassSheets[name] = newClass
 		// uncomment to print all character classes
-		// fmt.Println(newClass.ToString())
-	}
+		fmt.Println(newClass.ToString())
+		return true
+	})
 }
 
 func GetCharacterClass(name enum.CharacterClassName) (cc.CharacterClass, error) {
-	class, exists := characterClasses[name]
+
+	class, exists := characterClasses.Load(name)
 	if !exists {
 		return cc.CharacterClass{}, fmt.Errorf("character class %s not found", name)
 	}
-	return class, nil
+	return class.(cc.CharacterClass), nil
 }
 
 func GetAllCharacterClasses() map[enum.CharacterClassName]cc.CharacterClass {
-	return characterClasses
+	charClasses := make(map[enum.CharacterClassName]cc.CharacterClass)
+
+	characterClasses.Range(func(key, value interface{}) bool {
+		name := key.(enum.CharacterClassName)
+		class := value.(cc.CharacterClass)
+		charClasses[name] = class
+		return true
+	})
+	return charClasses
 }
