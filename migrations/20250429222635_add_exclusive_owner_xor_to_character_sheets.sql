7f85f51afc0c67e436347496d7c92d886b0dde29
422UR4H <tiagosindra@id.uff.br>
2025-05-08 20:44:34 -0300

    fix: change scenario_uuid to master_uuid in last migration

--- migrations/20250429222635_add_exclusive_owner_xor_to_character_sheets.sql
+++ migrations/20250429222635_add_exclusive_owner_xor_to_character_sheets.sql
@@ -4,16 +4,16 @@ BEGIN;
 
 ALTER TABLE character_sheets
 ADD COLUMN player_uuid UUID NULL,
-ADD COLUMN scenario_uuid UUID NULL;
+ADD COLUMN master_uuid UUID NULL;
 
 CREATE INDEX idx_character_sheets_player_uuid ON character_sheets (player_uuid);
-CREATE INDEX idx_character_sheets_scenario_uuid ON character_sheets (scenario_uuid);
+CREATE INDEX idx_character_sheets_master_uuid ON character_sheets (master_uuid);
 
 -- Add CHECK constraint to ensure uniqueness (logical XOR)
 ALTER TABLE character_sheets
 ADD CONSTRAINT chk_exclusive_owner 
-CHECK ((player_uuid IS NULL AND scenario_uuid IS NOT NULL) OR 
-       (player_uuid IS NOT NULL AND scenario_uuid IS NULL));
+CHECK ((player_uuid IS NULL AND master_uuid IS NOT NULL) OR 
+       (player_uuid IS NOT NULL AND master_uuid IS NULL));
 
 COMMIT;
 -- +goose StatementEnd
@@ -25,10 +25,10 @@ BEGIN;
 ALTER TABLE character_sheets DROP CONSTRAINT IF EXISTS chk_exclusive_owner;
 
 DROP INDEX IF EXISTS idx_character_sheets_player_uuid;
-DROP INDEX IF EXISTS idx_character_sheets_scenario_uuid;
+DROP INDEX IF EXISTS idx_character_sheets_master_uuid;
 
 ALTER TABLE character_sheets DROP COLUMN IF EXISTS player_uuid;
-ALTER TABLE character_sheets DROP COLUMN IF EXISTS scenario_uuid;
+ALTER TABLE character_sheets DROP COLUMN IF EXISTS master_uuid;
 
 COMMIT;
 -- +goose StatementEnd
