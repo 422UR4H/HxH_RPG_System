a56feb73f2df1a30791173ace9e0e7daa3665045
422UR4H <tiagosindra@id.uff.br>
2026-05-09 22:47:23 -0300

    fix: add happenedAt and returns slice copy for skills

--- internal/domain/entity/match/action/master_action.go
+++ internal/domain/entity/match/action/master_action.go
@@ -1,6 +1,10 @@
 package action
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type MasterAction struct {
 	TargetID    []uuid.UUID
@@ -8,10 +12,23 @@ type MasterAction struct {
 	Move        *Move
 	Attack      *Attack
 	ActionSpeed *RollCheck
+	happenedAt  time.Time
 	// Initiative *Initiative ?
 	// Penalidade *Penalty ?
 }
 
 func NewMasterAction() *MasterAction {
 	return &MasterAction{}
+}
+
+func (ma *MasterAction) GetHappenedAt() time.Time {
+	return ma.happenedAt
+}
+
+func (ma *MasterAction) GetSkills() []Skill {
+	// 1. create a new slice with the exact same length
+	skillsCopy := make([]Skill, len(ma.skills))
+	// 2. Copy the data from the original slice to the new one
+	copy(skillsCopy, ma.skills)
+	return skillsCopy
 }
